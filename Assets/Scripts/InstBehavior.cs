373fb574399cc44b3a9593275aa5ba7fddf7b2c1
Ali Egseem <aliegseem@gmail.com>
2017-11-06 16:16:41 -0500

    Test scene improved, added 'phase 2' of 3

--- Assets/Scripts/InstBehavior.cs
+++ Assets/Scripts/InstBehavior.cs
@@ -8,25 +8,36 @@ public class InstBehavior : MonoBehaviour {
     [SerializeField] Light deskLight;
     [SerializeField] [Range(1f, 200f)] float drawDistance = 50f;
     [SerializeField] Light spotlight;
+    [SerializeField] Light backLight;
+
 
     GameObject drawObject;
     bool drawing = false;
     Vector3 mouseStartingPosition;
     bool cubeClicked = false;
     Camera cam;
+    float sizeMorphing = 0f;
+    Animator anim;
 
     void Start()
     {
         cam = GetComponent<Camera>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
+        drawObject = prefab as GameObject;
+
+        if (sizeMorphing == 1)
+            sizeMorphing += 0.01f;
+        else
+            sizeMorphing -= 0.0f;
+
         deskLight.transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
 
         if (Input.GetMouseButtonDown(0))
         {
-            drawObject = prefab as GameObject;
             mouseStartingPosition = Input.mousePosition;
             Ray ray = cam.ScreenPointToRay(mouseStartingPosition);
             RaycastHit hit;
@@ -35,9 +46,9 @@ public class InstBehavior : MonoBehaviour {
             {
                 if (hit.transform.name == "frontPlane")
                 {
-                    drawObject.GetComponent<Rigidbody>().AddForce(new Vector3(00, 500, 0));
+                    drawObject.GetComponent<Rigidbody>().AddForce(new Vector3(10, 500, 0));
                 }
-                else if (hit.transform.name == "Cube")
+                else if (hit.transform.name == "mouseCube")
                 {
                     foreach (Collider c in topPlane.GetComponents<Collider>())
                     {
@@ -45,6 +56,7 @@ public class InstBehavior : MonoBehaviour {
                     }
 
                     cubeClicked = true;
+                    anim.SetBool("scene2", true);
                 }
             }
         }
@@ -53,9 +65,10 @@ public class InstBehavior : MonoBehaviour {
         {
 
             spotlight.color = Color.white;
-            spotlight.intensity = 33f;
+            spotlight.intensity = 10f;
             spotlight.range = 12;
 
+            backLight.color = Color.cyan;
             deskLight.color = Color.blue;
         }
     }
