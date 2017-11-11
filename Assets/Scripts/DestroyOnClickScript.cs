6224f5bcac0adef89ebeabe415c284ffc3a76d95
Ali <aliegseem@gmail.com>
2017-11-11 03:43:19 -0500

    Finished first pass, feature complete

--- Assets/Scripts/DestroyOnClickScript.cs
+++ Assets/Scripts/DestroyOnClickScript.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class DestroyOnClickScript : MonoBehaviour
 {
 
+    [SerializeField] GameObject table;
+    [SerializeField] GameObject computer;
+    [SerializeField] GameObject text;
+    [SerializeField] GameObject particle1;
+    [SerializeField] GameObject particle2;
+    [SerializeField] GameObject endParticle;
+
     // Use this for initialization
     void Start()
     {
@@ -22,8 +29,15 @@ public class DestroyOnClickScript : MonoBehaviour
             if (Physics.Raycast(ray, out hit))
             {
                 Debug.Log(hit.transform);
-                if (hit.transform.tag == "letterDestroy")
+                if (hit.transform.name == "BigText")
                 {
+                    table.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+                    computer.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+                    text.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+                    particle1.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+                    particle2.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+                    endParticle.GetComponent<Animator>().SetBool("flipUp", true);
+
                     Destroy(gameObject);
                 }
             }
